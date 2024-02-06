59da1b45a9a0378c40640964cae7361aacf9cc4c
Rishav Das <rishavdas27602@gmail.com>
2024-02-06 12:59:49 +0530

    first commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color bgOver = Color.fromARGB(255, 244, 219, 181);
+const Color bgUnder = Color.fromARGB(255, 240, 175, 171);
+const Color bgNorm = Color.fromARGB(255, 138, 235, 203);
+const Color bgAppBar = Color.fromARGB(255, 25, 5, 66);
+const Color bgDefault = Color.fromARGB(255, 179, 188, 240);
---
+++ lib/main.dart
@@ -0,0 +1,145 @@
+import 'package:bmi_app/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterApp',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: bgAppBar),
+        useMaterial3: true,
+      ),
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  MyHomePageState createState() => new MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  var wtController = TextEditingController();
+  var ftController = TextEditingController();
+  var inController = TextEditingController();
+  var result = "";
+  var bgColor = bgDefault;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'Your-BMI',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Container(
+        color: bgColor,
+        child: Center(
+          child: Container(
+            width: 300,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'BMI',
+                  style: TextStyle(fontSize: 43, fontWeight: FontWeight.w700),
+                ),
+                TextField(
+                  controller: wtController,
+                  decoration: InputDecoration(
+                      label: Text('Enter your Weight(in Kgs)'),
+                      prefixIcon: Icon(Icons.line_weight)),
+                  keyboardType: TextInputType.number,
+                ),
+                SizedBox(
+                  height: 11,
+                ),
+                TextField(
+                  controller: ftController,
+                  decoration: InputDecoration(
+                      label: Text('Enter your Height(in feet)'),
+                      prefixIcon: Icon(Icons.height)),
+                  keyboardType: TextInputType.number,
+                ),
+                SizedBox(
+                  height: 11,
+                ),
+                TextField(
+                  controller: inController,
+                  decoration: InputDecoration(
+                      label: Text('Enter your Height(in inch)'),
+                      prefixIcon: Icon(Icons.height)),
+                  keyboardType: TextInputType.number,
+                ),
+                SizedBox(
+                  height: 11,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      var wt = wtController.text.toString();
+                      var ft = ftController.text.toString();
+                      var inch = inController.text.toString();
+                      if (wt != "" && ft != "" && inch != "") {
+                        var iWt = int.parse(wt);
+                        var iFt = int.parse(ft);
+                        var iInch = int.parse(inch);
+
+                        var tInch = (iFt * 12) + iInch;
+                        var tCm = (tInch * 2.54);
+                        var tM = tCm / 100;
+                        var bmi = (iWt / (tM * tM));
+                        var msg = "";
+                        if (bmi > 25) {
+                          msg = "You're OverWeight!!";
+                          bgColor = bgOver;
+                        } else if (bmi < 18) {
+                          msg = "You're UnderWeight!!";
+                          bgColor = bgUnder;
+                        } else {
+                          msg = "You're Healthy!!";
+                          bgColor = bgNorm;
+                        }
+                        setState(() {
+                          result =
+                              "${msg} \nYour BMI is : ${bmi.toStringAsFixed(4)}";
+                        });
+                        // if()
+                      } else {
+                        setState(() {
+                          result = "Please fill all the required spaces!!";
+                        });
+                      }
+                    },
+                    child: Text('Calculate')),
+                SizedBox(
+                  height: 11,
+                ),
+                Text(
+                  '$result',
+                  style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
